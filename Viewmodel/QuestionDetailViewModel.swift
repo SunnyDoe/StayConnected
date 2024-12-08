15c8ee6642c91e539b3c8255f022a7e10275d2fe
irakli <46853093+iraklijanashvili@users.noreply.github.com>
2024-12-08 15:24:34 +0400

    Update QuestionDetailViewModel.swift

--- Viewmodel/QuestionDetailViewModel.swift
+++ Viewmodel/QuestionDetailViewModel.swift
@@ -5,7 +5,6 @@
 //  Created by Imac on 07.12.24.
 //
 
-
 import Foundation
 import Combine
 
@@ -20,12 +19,17 @@ class QuestionDetailViewModel {
     
     init(question: Question) {
         self.question = question
+        print("QuestionDetailViewModel initialized with question ID: \(String(describing: question.id))")
     }
     
     func fetchAnswers() {
-        guard let questionId = question.id else { return }
+        guard let questionId = question.id else {
+            print("Cannot fetch answers: Question ID is nil")
+            return
+        }
         
         isLoading = true
+        print("Fetching answers for question ID: \(questionId)")
         
         networkManager.fetchSpecificQuestionAnswers(questionId: questionId) { [weak self] result in
             DispatchQueue.main.async {
@@ -33,29 +37,36 @@ class QuestionDetailViewModel {
                 
                 switch result {
                 case .success(let answers):
+                    print("Successfully fetched \(answers.count) answers")
                     self?.answers = answers
                 case .failure(let error):
-                    self?.error = error
                     print("Error fetching answers: \(error.localizedDescription)")
+                    self?.error = error
                 }
             }
         }
     }
     
     func sendAnswer(_ answerText: String, completion: @escaping (Bool) -> Void) {
+        print("Attempting to send answer: \(answerText)")
+        
         guard let questionId = question.id else {
+            print("Cannot send answer: Question ID is nil")
             completion(false)
             return
         }
+        
+        print("Sending answer for question ID: \(questionId)")
         
         networkManager.sendAnswer(questionId: questionId, answerText: answerText) { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let answer):
+                    print("Answer sent successfully: \(answer)")
                     self?.answers.append(answer)
                     completion(true)
                 case .failure(let error):
-                    print("Failed to send answer: \(error.localizedDescription)")
+                    print("Failed to send answer. Error: \(error.localizedDescription)")
                     completion(false)
                 }
             }
